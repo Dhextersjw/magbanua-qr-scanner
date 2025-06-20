e81e5f45fe8095b2d760c3b4bcff9fa81e939736
Dhextersjw <magbanuadhexter59@gmail.com>
2025-06-20 08:44:03 +0800

    update new text file

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       </v-card>
       <v-card>
         <v-card-title class="headline">
-          WELCOME fuck you luis
+          HELLO SAY GOODBYE
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
